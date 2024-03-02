9237b0391dbc026a11042777f929a25ed2f1afea
jun-1256 <juns_1201@icloud.com>
2024-03-02 23:02:10 +0900

    2024/03/02

---
+++ TestProject02/src/assignment4.java
@@ -0,0 +1,12 @@
+
+public class assignment4 {
+
+	public static void main(String[] args) {
+		// 変数と代入
+		//数字は、intで処理する。
+		int num1 = 12345;
+		System.out.println(num1);
+		
+	}
+
+}
---
+++ TestProject02/src/variable.java
@@ -0,0 +1,19 @@
+
+public class variable {
+
+	public static void main(String[] args) {
+		// 変数と代入
+		//プリミティブ型(基本的な型) 04_Java p.5参照
+		//文字列を格納できるプリミティブ型はない → Stringクラスを参照して型を定義
+		//Stringクラス → "Java.lang"というパッケージの中にあるAPI
+		//JavaにおけるAPI → クラスライブラリ
+		//つまり、Stringクラス=クラスライブラリのこと。
+		int suugaku = 45;
+		int eigo = 82;
+		if (suugaku > 50 || eigo > 50) {
+			System.out.println("この人は合格です");
+			
+		}
+	}
+
+}

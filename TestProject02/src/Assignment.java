2e5cce63a3dad9564e3dc84e1cec00da544531c6
jun-1256 <juns_1201@icloud.com>
2024-03-06 09:24:51 +0900

    2024/03/02

---
+++ TestProject02/src/Assignment.java
@@ -0,0 +1,16 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// 変数と代入
+		//代入は = で書く
+		//変数nameに表示したいものを代入して、nameをSystem .out.printlnで出力
+		//(例)String name = "高橋";
+		//(例)System.out.println(name);
+		//実行結果では、高橋と表示される。
+		String str1 = "鈴木";
+		System.out.println(str1);
+
+	}
+
+}
---
+++ TestProject02/src/Assignment5.java
@@ -0,0 +1,12 @@
+
+class Assignment5 {
+
+	public static void main(String[] args) {
+		// 変数と代入
+		//真偽値は、booleanで処理する。
+		boolean boo = true;
+		System.out.println(boo);
+
+	}
+
+}
